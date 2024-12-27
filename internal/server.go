0748b3ae03a6335e9b5650bb1cd8187415f9687c
𝐘𝐨𝐬𝐞𝐛𝐲𝐭𝐞 <54016243+yosebyte@users.noreply.github.com>
2024-12-27 17:26:26 +0800

    Update server.go

--- internal/server.go
+++ internal/server.go
@@ -24,40 +24,41 @@ func NewServer(parsedURL *url.URL) *http.Server {
 }
 
 func handleServerRequest(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodConnect {
-		log.Debug("User-Agent: %v", r.Header.Get("User-Agent"))
-		if r.Header.Get("User-Agent") != getagentID() {
-			statusOK(w)
-		}
-		clientConn, err := hijackConnection(w)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			log.Error("Unable to hijack connection: %v", err)
-			return
-		}
-		log.Debug("Client connected: %v", clientConn.RemoteAddr())
-		defer func() {
-			if clientConn != nil {
-				clientConn.Close()
-			}
-		}()
-		targetConn, err := net.Dial("tcp", r.URL.Host)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusServiceUnavailable)
-			log.Error("Unable to dial target: %v", err)
-			return
-		}
-		log.Debug("Target connected: %v", targetConn.RemoteAddr())
-		defer func() {
-			if targetConn != nil {
-				targetConn.Close()
-			}
-		}()
-		log.Debug("Connection established: %v <-> %v", clientConn.RemoteAddr(), targetConn.RemoteAddr())
-		if err := io.DataExchange(clientConn, targetConn); err != nil {
-			log.Debug("Connection closed: %v", err)
-		}
-	} else {
+	if r.Method != http.MethodConnect {
 		statusOK(w)
+		log.Warn("Invalid method: %v/%v", r.RemoteAddr, r.Method)
+		return
+	}
+	log.Debug("User-Agent: %v", r.Header.Get("User-Agent"))
+	if r.Header.Get("User-Agent") != getagentID() {
+		statusOK(w)
+	}
+	clientConn, err := hijackConnection(w)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Error("Unable to hijack connection: %v", err)
+		return
+	}
+	log.Debug("Client connected: %v", clientConn.RemoteAddr())
+	defer func() {
+		if clientConn != nil {
+			clientConn.Close()
+		}
+	}()
+	targetConn, err := net.Dial("tcp", r.URL.Host)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+		log.Error("Unable to dial target: %v", err)
+		return
+	}
+	log.Debug("Target connected: %v", targetConn.RemoteAddr())
+	defer func() {
+		if targetConn != nil {
+			targetConn.Close()
+		}
+	}()
+	log.Debug("Connection established: %v <-> %v", clientConn.RemoteAddr(), targetConn.RemoteAddr())
+	if err := io.DataExchange(clientConn, targetConn); err != nil {
+		log.Debug("Connection closed: %v", err)
 	}
 }
