6aeff8e2591158297c123560d813b9a8692a29cb
𝐘𝐨𝐬𝐞𝐛𝐲𝐭𝐞 <54016243+yosebyte@users.noreply.github.com>
2024-12-27 20:02:02 +0800

    Update core.go

--- cmd/atlas/core.go
+++ cmd/atlas/core.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"net/http"
 	"net/url"
 	"os"
@@ -10,12 +9,12 @@ import (
 	"github.com/yosebyte/x/log"
 )
 
-func coreSelect(parsedURL *url.URL, stop chan os.Signal) {
+func coreSelect(parsedURL *url.URL) {
 	switch parsedURL.Scheme {
 	case "server":
-		runServer(parsedURL, stop)
+		runServer(parsedURL)
 	case "client":
-		runClient(parsedURL, stop)
+		runClient(parsedURL)
 	default:
 		log.Error("Invalid scheme: %v", parsedURL.Scheme)
 		helpInfo()
@@ -23,34 +22,18 @@ func coreSelect(parsedURL *url.URL, stop chan os.Signal) {
 	}
 }
 
-func runServer(parsedURL *url.URL, stop chan os.Signal) {
+func runServer(parsedURL *url.URL) {
 	log.Info("Server started: %v", parsedURL.String())
 	server := internal.NewServer(parsedURL)
-	go func() {
-		if err := server.ListenAndServeTLS("", ""); err != nil && err != http.ErrServerClosed {
+	if err := server.ListenAndServeTLS("", ""); err != nil {
 			log.Error("Server error: %v", err)
 		}
-	}()
-	<-stop
-	log.Info("Server stopping")
-	if err := server.Shutdown(context.Background()); err != nil {
-		log.Error("Server shutdown error: %v", err)
-	}
-	log.Info("Server stopped")
 }
 
 func runClient(parsedURL *url.URL, stop chan os.Signal) {
 	log.Info("Client started: %v", parsedURL.String())
 	client := internal.NewClient(parsedURL)
-	go func() {
-		if err := client.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+	if err := client.ListenAndServe(); err != nil {
 			log.Error("Client error: %v", err)
 		}
-	}()
-	<-stop
-	log.Info("Client stopping")
-	if err := client.Shutdown(context.Background()); err != nil {
-		log.Error("Client shutdown error: %v", err)
-	}
-	log.Info("Client stopped")
 }
