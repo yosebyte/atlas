f620ac8494f6aeb46e223b3c0ec1606cba99b024
𝐘𝐨𝐬𝐞𝐛𝐲𝐭𝐞 <54016243+yosebyte@users.noreply.github.com>
2025-01-12 22:36:02 +0800

    Update common.go

--- internal/common.go
+++ internal/common.go
@@ -7,6 +7,8 @@ import (
 	"time"
 )
 
+var agentPrefix = "nghttp2/"
+
 func hijackConnection(w http.ResponseWriter) (net.Conn, error) {
 	hijacker, ok := w.(http.Hijacker)
 	if !ok {
@@ -17,5 +19,27 @@ func hijackConnection(w http.ResponseWriter) (net.Conn, error) {
 }
 
 func getagentID() string {
-	return "atlas/" + strconv.FormatInt(time.Now().Truncate(time.Minute).Unix(), 16)
+	return agentPrefix + strconv.FormatInt(time.Now().Truncate(time.Minute).Unix(), 16)
 }
+
+const pageNotFound = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404 Not Found</title>
+    <style>
+        body {
+			font-family: Courier, monospace;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <h1>404 Not Found</h1>
+    <p>The page you are looking for does not exist.</p>
+	<a href="/">Try again</a>
+</body>
+</html>
+`
